4be423f9b93eab970f9fd1ac415e2f17a7e8dcc9
Matthew Endsley <mendsley@gmail.com>
2012-08-15 23:48:22 -0700

    fixing up platforms

--- premake/platforms.lua
+++ premake/platforms.lua
@@ -26,33 +26,11 @@ newplatform {
 
 newplatform {
 	name = "nacl64",
-	description = "Chrome NativeClient",
+	description = "Chrome 64bit NativeClient",
 	gcc = {
 		cc = "x86_64-nacl-gcc",
 		cxx = "x86_64-nacl-g++",
 		ar = "x86_64-nacl-ar",
 		cppflags = "-MMD -m64",
-	}
-}
-
-newplatform {
-	name ="linux-alsa",
-	description = "Linux via ALSA",
-	gcc = {
-		cc = "gcc",
-		cxx = "g++",
-		ar = "ar",
-		cppflags = "-MMD",
-	}
-}
-
-newplatform {
-	name ="linux-pulse",
-	description = "Linux via pulse sound server",
-	gcc = {
-		cc = "gcc",
-		cxx = "g++",
-		ar = "ar",
-		cppflags = "-MMD",
 	}
 }
